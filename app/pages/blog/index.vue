09015a2dc91dbad5e50ed0987367db6a43a50924
mahsankhan <55183761+mahsankhan@users.noreply.github.com>
2020-03-28 07:08:27 +0500

    Update index.vue

--- app/pages/blog/index.vue
+++ app/pages/blog/index.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="blog">
     <div class="py-8 md:py-16 text-center">
-      <h1 class="text-lg md:text-xl lg:text-4xl xl:text-6xl">Blog</h1>
+      <h1 class="text-lg md:text-xl lg:text-4xl xl:text-6xl">Latest News</h1>
       <h2 class="text-base md:text-lg lg:text-xl xl:text-2xl">
 You can find here all happenings about MEASA DeFi.      </h2>
     </div>
